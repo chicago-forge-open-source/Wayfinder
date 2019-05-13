73932687babff891965368a38e9180cdea2952c9
Natalie Frecka <natalie.frecka@accenture.com>
2019-05-13 17:01:41 -0500

    Basic tests for determining if user has made valid screen touch
    Signed-off-by: Riley Moss <rmoss@pillartechnology.com>

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using GoogleARCore.Examples.Common;
 
 public class CameraScript : MonoBehaviour {
 	public Camera firstPersonCamera;
-
-
+//	public GameObject DetectedPlanePrefab;
+	public GameObject AnchorPlanePrefab;
+	public GameObject AnchorPointPrefab;
+	private const float k_ModelRotation = 180.0f;
 
 	void Start () {
 		
@@ -13,6 +17,75 @@ public class CameraScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// something 
+		_update ();
+	}
+
+	public bool IsNotValidTouch(int touchCount, TouchPhase phase)
+	{
+		return touchCount < 1 || phase != TouchPhase.Began;
+	}
+
+	// Detect if user touches screen
+		// if not do nothing
+
+	// 
+
+
+	private void _update() {
+		// If the player has not touched the screen, we are done with this update.
+//		Touch touch;
+//		if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+//		{
+			return;
+//		}
+//
+//		// Should not handle input if the player is pointing on UI.
+//		if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+//		{
+//			return;
+//		}
+//
+//		// Raycast against the location the player touched to search for planes.
+//		TrackableHit hit;
+//		TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+//			TrackableHitFlags.FeaturePointWithSurfaceNormal;
+//
+//		if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+//		{
+//			// Use hit pose and camera pose to check if hittest is from the
+//			// back of the plane, if it is, no need to create the anchor.
+//			if ((hit.Trackable is DetectedPlane) &&
+//				Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+//					hit.Pose.rotation * Vector3.up) < 0)
+//			{
+//				Debug.Log("Hit at back of the current DetectedPlane");
+//			}
+//			else
+//			{
+//				// Choose the Andy model for the Trackable that got hit.
+//				GameObject prefab;
+//				if (hit.Trackable is FeaturePoint)
+//				{
+//					prefab = AnchorPointPrefab;
+//				}
+//				else
+//				{
+//					prefab = AnchorPlanePrefab;
+//				}
+//
+//				// Instantiate Andy model at the hit pose.
+//				var andyObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
+//
+//				// Compensate for the hitPose rotation facing away from the raycast (i.e.
+//				// camera).
+//				andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
+//
+//				// Create an anchor to allow ARCore to track the hitpoint as understanding of
+//				// the physical world evolves.
+//				var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+//				// Make Andy model a child of the anchor.
+//				andyObject.transform.parent = anchor.transform;
+//			}
+//		}
 	}
 }
---
+++ Assets/Editor/CameraSceneTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Editor
+{
+	public class CameraSceneTest {
+
+		[Test]
+		public void GivenTouchCountIsZeroAndInvalidTouchPhaseShouldReturnTrue()
+		{
+			var gameObject = new GameObject();
+			var script = gameObject.AddComponent<CameraScript>();
+			var touched = script.IsNotValidTouch (0, TouchPhase.Ended);
+			Assert.True(touched);
+		}
+
+		[Test]
+		public void GivenTouchCountIsZeroAndValidTouchPhaseShouldReturnTrue()
+		{
+			var gameObject = new GameObject();
+			var script = gameObject.AddComponent<CameraScript>();
+			var touched = script.IsNotValidTouch (0, TouchPhase.Began);
+			Assert.True(touched);
+		}
+		
+		[Test]
+		public void GivenTouchCountGreaterThanZeroAndInvalidTouchPhaseShouldReturnTrue()
+		{
+			var gameObject = new GameObject();
+			var script = gameObject.AddComponent<CameraScript>();
+			var touched = script.IsNotValidTouch (1, TouchPhase.Ended);
+			Assert.True(touched);
+		}
+
+		[Test]
+		public void GivenTouchCountGreaterThanZeroAndValidTouchPhaseShouldReturnFalse()
+		{
+			var gameObject = new GameObject();
+			var script = gameObject.AddComponent<CameraScript>();
+			var touched = script.IsNotValidTouch(1, TouchPhase.Began);
+			Assert.False(touched);
+		}
+	}
+}
